c5c6b7ae9bcfb9794eb0c1215ee5eaf8ad963b27
Guest User <Guest@Puneets-MacBook-Pro.local>
2025-03-18 16:30:51 -0400

    vhbj

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 // Student Information
 $student_name = "Jai Arrora";
 $student_id = "500234620";
+$student_name = "Kunal Arrora";
+$student_id = "5002356778";
 
 // Display student information
 echo "Student Name: " . $student_name . "<br>";
