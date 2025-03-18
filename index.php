b96340806540cf778d512c603432e94c4c75f4e4
Guest User <Guest@Puneets-MacBook-Pro.local>
2025-03-18 16:16:47 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Student Information
+$student_name = "Jai Arrora";
+$student_id = "500234620";
+
+// Display student information
+echo "Student Name: " . $student_name . "<br>";
+echo "Student ID: " . $student_id;
+?>
+
